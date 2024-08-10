64bb19ecab4d160466d8416929195bf185a1d04f
Pramish K.C <pramishkc066@gmail.com>
2024-08-10 15:09:50 +0545

    added flip mechanism

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+dataframe = pd.read_csv("data/french_words.csv")
+data_list = dataframe.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(data_list)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=front)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
 
 window = Tk()
 window.title("Flash_Card")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front)
+back = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=front)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=1, column=1)
+canvas.grid(row=0, column=0, columnspan=2)
 
 right = PhotoImage(file="images/right.png")
-right_button = Button(image=right)
-right_button.config(highlightthickness=0)
-right_button.grid(row=2, column=1)
+known_button = Button(image=right, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
 
-wrong = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong)
-wrong_button.config(highlightthickness=0)
-wrong_button.grid(row=2, column=1)
+wrong_img = PhotoImage(file="images/wrong.png")
+unknown = Button(image=wrong_img, highlightthickness=0, command=next_card)
+unknown.grid(row=1, column=0)
 
+next_card()
 window.mainloop()
